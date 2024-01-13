35f90ab29e3be54a03c94e62b3d7fa064130d616
Nathanael-Tekalign <156130272+Nathanael-Tekalign@users.noreply.github.com>
2024-01-13 14:35:32 -0600

    Create TesterFile.js

---
+++ TesterFile.js
@@ -0,0 +1,10 @@
+/*
+Author: Nathanael Tekalign
+Date: Janurary 14th, 2024
+*/
+var myVariableInteger_0 = 57;
+var myVariableInteger_0 = myVariableInteger_0 + 1;
+let myVariableString_0 = myVariableInteger_0.toString();
+let favoriteThing = prompt("What is your favorite thing?");
+console.log('myVariableString_0:', myVariableString_0);
+console.log('favoriteThing:', favoriteThing);
