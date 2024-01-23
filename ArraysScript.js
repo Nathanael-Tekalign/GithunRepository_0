50dd9a0a63413606c912f2f2798ed433024983bd
Nathanael-Tekalign <156130272+Nathanael-Tekalign@users.noreply.github.com>
2024-01-22 21:13:11 -0600

    Update ArraysScript.js

--- ArraysScript.js
+++ ArraysScript.js
@@ -4,3 +4,15 @@ const addOne = function(x) {
 }
 var addOne_String = addOne.toString();
 console.log('addOne_String', addOne_String);
+function myFunction(func,addOne) {
+  if(addOne % 2 === 0) {
+    func === addOne;
+  }
+  else {
+    console.log("this number is odd")
+  }
+}
+let txt = "";
+for(let x in Array1){
+  txt += Array1[x];
+}
